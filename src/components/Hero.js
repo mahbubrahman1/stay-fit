1f678e03cd72f77bf4ceda632d4982d3cc28ddb9
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-12-25 23:00:00 +0600

    added hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div style={{ backgroundImage: `url('https://images.pexels.com/photos/136404/pexels-photo-136404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`, backgroundSize: 'cover' }}>
+            <h1>GET FIT WITH US</h1>
+        </div>
+    )
+}
+
+export default Hero
