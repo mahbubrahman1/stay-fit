9e7f09bc6e1a904ffa5b33e3195206ebf6fae50c
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2023-01-06 11:15:38 +0600

    added all class section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Hero from "./components/Hero";
+import Classes from "./pages/Classes";
 import PopularClasses from "./pages/Home/PopularClasses";
 import Trainers from "./pages/Home/Trainers";
 
 function App() {
   return (
     <div>
-      <Hero />
-      <PopularClasses />
-      {/* <Trainers /> */}
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/classes" element={<Classes />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Classes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Classes = () => {
+    return (
+        <div className='bg-dark classes'>
+            <h2 className='text-light text-center'>Classes</h2>
+        </div>
+    )
+}
+
+export default Classes
